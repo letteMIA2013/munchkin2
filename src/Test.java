e996657bfdfb28b963af36f2324e75ced4efadf7
e3_bundesmann <user@gibts.net>
2014-06-11 09:57:54 +0200

    Stevens erster commit

--- src/Test.java
+++ src/Test.java
@@ -6,4 +6,8 @@
  * To change this template use File | Settings | File Templates.
  */
 public class Test {
+
+    public static void main(String[] args) {
+        System.out.println("Hier ist Steven!");
+    }
 }
